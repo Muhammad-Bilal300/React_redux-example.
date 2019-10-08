29eff0239daf0db5de2f992c6b778b745c81a62b
Muhammad_Bilal <bilalaarbi300@gmail.com>
2019-10-08 16:10:17 -0700

    Updates

---
+++ precache-manifest.ad7a732f9a1be54d7553358e875d9097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd493509a48911ca4cb88da54de382e",
+    "url": "/React_redux-example./index.html"
+  },
+  {
+    "revision": "6a06defa8f76fd94a7b0",
+    "url": "/React_redux-example./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1df8c921d40bf996b41b",
+    "url": "/React_redux-example./static/js/2.e053b764.chunk.js"
+  },
+  {
+    "revision": "6a06defa8f76fd94a7b0",
+    "url": "/React_redux-example./static/js/main.05ecba78.chunk.js"
+  },
+  {
+    "revision": "4fb9cb1bb8a7136e61d9",
+    "url": "/React_redux-example./static/js/runtime-main.9277d8db.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React_redux-example./precache-manifest.ad7a732f9a1be54d7553358e875d9097.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React_redux-example./index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
